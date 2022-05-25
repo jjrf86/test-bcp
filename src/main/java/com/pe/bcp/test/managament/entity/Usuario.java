ad5d46cba6fe026e2fb6741c8c32bca70d813655
Jesse James Rios Franco <jjrf86@gmail.com>
2022-05-25 04:27:22 -0500

    cambios en la tabla usuario y fix de depencia cruzada

--- src/main/java/com/pe/bcp/test/managament/entity/Usuario.java
+++ src/main/java/com/pe/bcp/test/managament/entity/Usuario.java
@@ -2,6 +2,7 @@ package com.pe.bcp.test.managament.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 
